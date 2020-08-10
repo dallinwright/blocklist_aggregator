9a476f6b6a6bfc1baab0d599e8bbeda25c589625
Dallin Wright <dallinwright01@gmail.com>
2020-08-10 19:27:46 +0200

    working git clone

--- src/index.ts
+++ src/index.ts
@@ -12,32 +12,30 @@ const createResponse = (statusCode: number, body: any) => ({
     body: JSON.stringify(body)
 });
 
-const cloneRepo = async () => {
+async function cloneRepo() {
     if (process.env.BASE_REPO) {
         const repo = process.env.BASE_REPO;
         const gitPath = repo.slice(repo.lastIndexOf('/') + 1);
         const path = gitPath.slice(0, gitPath.lastIndexOf('.'));
-        console.log(path);
+
         shell.rm('-rf', path);
         shell.exec(`git clone ${process.env.BASE_REPO}`);
 
         if (shell.error()) {
-            return createResponse(503, shell.error());
-        } else {
-            return createResponse(200, 'ok');
+            throw new Error(shell.error());
         }
     } else {
-        return createResponse(503, 'No Repo given for ip blacklist baseline');
-
+        throw new Error("No Repo given for ip blacklist baseline.");
     }
 }
 
 
 export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
     try {
-        return await cloneRepo();
+        await cloneRepo();
+        return createResponse(200, 'ok');
     } catch (e) {
-        return createResponse(503, e);
+        return createResponse(503, e.message);
     }
 };
 
