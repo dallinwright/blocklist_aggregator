d75c4388812da75cf7edf1dad034876d83cc94c3
Dallin Wright <dallinwright01@gmail.com>
2020-08-10 18:54:44 +0200

    complete clone logic, need to work on adding err handling, plus a lot more

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import {Callback, Context, Handler} from 'aws-lambda';
-import {APIResponse} from "./interfaces/APIResponse";
 
 const shell = require('shelljs');
 
@@ -13,13 +12,32 @@ const createResponse = (statusCode: number, body: any) => ({
     body: JSON.stringify(body)
 });
 
+const cloneRepo = async () => {
+    if (process.env.BASE_REPO) {
+        const repo = process.env.BASE_REPO;
+        const gitPath = repo.slice(repo.lastIndexOf('/') + 1);
+        const path = gitPath.slice(0, gitPath.lastIndexOf('.'));
+        console.log(path);
+        shell.rm('-rf', path);
+        shell.exec(`git clone ${process.env.BASE_REPO}`);
+
+        if (shell.error()) {
+            return createResponse(503, shell.error());
+        } else {
+            return createResponse(200, 'ok');
+        }
+    } else {
+        return createResponse(503, 'No Repo given for ip blacklist baseline');
+
+    }
+}
+
 
 export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
-    const response: APIResponse = createResponse(200, event);
-
-
-    shell.exec(`git clone ${process.env.BASE_REPO}`);
-
-    return await response;
+    try {
+        return await cloneRepo();
+    } catch (e) {
+        return createResponse(503, e);
+    }
 };
 
