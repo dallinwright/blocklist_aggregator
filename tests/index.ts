874c4ffd97906db91a73e22c625968be430e9c53
Dallin Wright <dallinwright01@gmail.com>
2020-08-12 08:57:32 +0200

    adding github workflow and test

--- tests/index.ts
+++ tests/index.ts
@@ -1,4 +1,4 @@
-import {cloneRepo} from "../src";
+import {cloneRepo, getMatchingFiles} from "../src";
 import {expect} from 'chai';
 import chai = require('chai');
 
@@ -7,12 +7,21 @@ chai.use(require('chai-as-promised'))
 describe('Lambda Handler Tests', () => { // the tests container
     describe('Repo Clone', () => {
         it('Should not clone an undefined repo', async () => {
-            await expect(cloneRepo(undefined)).to.be.rejectedWith(Error)
+            await expect(cloneRepo()).to.be.rejectedWith(Error)
         })
 
         it('Should clone a given repo, even if it already exists', async () => {
-            const repo = 'https://github.com/dallinwright/blocklist_aggregator.git';
-            await expect(await cloneRepo(repo)).to.eq('blocklist_aggregator');
-        }).timeout(10000);
+            process.env.BASE_REPO = 'https://github.com/dallinwright/blocklist_aggregator.git';
+            await expect(await cloneRepo()).to.eq('blocklist_aggregator');
+        }).timeout(20000);
+    });
+
+    describe('Get matching files', () => {
+        it('Should return a list of files matching the path and match pattern', async () => {
+            const path = 'blocklist_aggregator/test';
+            process.env.BLOCK_LISTS = "test.ipset";
+            const matchingFiles = getMatchingFiles(path);
+            expect(matchingFiles).to.eq([path + '/test.ipset']);
+        })
     });
 });
